29be4d96ef959d748550a812845727a775206e18
Joseph Kato <joseph@jdkato.io>
2021-10-07 14:39:15 -0700

    Update db

--- README.md
+++ README.md
@@ -5,14 +5,14 @@
 ### ℹ️ Introduction
 
 The voting process for the NBA's most prestigious awards&mdash;such as *MVP*,
-*DPOY*, and its *All-NBA Teams*&mdash;has always been fairly ill-defined in
-terms of both *who* votes and *how* they voted.
+*DPOY*, and its *All-NBA Teams*&mdash;has always been [fairly ill-defined][4]
+in terms of both *who* votes and *how* they voted.
 
 The process itself has [changed drastically][1] over the years and the full
-voting results weren't even published until the 2014 - 2015 season (a
+voting results weren't even published until the 2015 season (a
 [*certain ballot*][2] in particular might have had something to do with that).
 
-Since that 2014 season, the NBA has released [PDF-formatted tables][3] of the
+Since that 2015 season, the NBA has released [PDF-formatted tables][3] of the
 results as tabulated by Ernst &amp; Young LLP. And while this was certainly
 a major step forward in transparency, the format isn't very useful for data
 analysis.
@@ -22,3 +22,4 @@ We're changing that.
 [1]: https://hoopshype.com/2020/07/11/media-nba-awards-vote/
 [2]: http://grantland.com/the-triangle/unanimous-animus-the-lebron-james-mvp-vote-and-debunking-the-myths-of-value/
 [3]: https://pr.nba.com/voting-results-2020-21-nba-regular-season-awards/
+[4]: https://www.masslive.com/celtics/2021/06/jayson-tatum-wants-changes-to-all-nba-voting-criteria-after-costly-snub-during-career-year.html
--- app.py
+++ app.py
@@ -8,15 +8,14 @@ from tinydb import TinyDB, where, Query
 from collections import defaultdict
 
 DB = TinyDB("db.json")
-
 YEARS = [
-    2014,
     2015,
     2016,
     2017,
     2018,
     2019,
     2020,
+    2021,
 ]
 
 
@@ -55,31 +54,106 @@ if __name__ == "__main__":
     # Intro
     st.markdown(pathlib.Path("README.md").read_text())
 
+    st.subheader("❓ Why is this important?")
+    st.markdown(
+        f"""
+
+        In 2016, the NBA players and Board of Governors ratified a new
+        [Collective Bargaining Agreement][2]. This agreement included a
+        "designated player" exception that allows a team to sign one of its own
+        players to a five-year maximum contract extension, according to the
+        [following criteria][4]:
+
+
+        > 1. He makes one of the three all-NBA teams or is named either
+             defensive player of the year or most valuable player the previous
+             season.
+        > 2. He has made one of the three all-NBA teams or has been named
+            defensive player of the year in two of the prior three seasons or
+            the league’s most valuable player in one of the three prior
+            seasons.
+
+        This is exception comes in addition to the well-known
+        "[Derrick Rose Rule][3]," which incentivizes making All-NBA teams
+        during a player's first four years. In response to the heightened
+        stakes of its media-based awards, the NBA also made a [few changes][1]
+        to its voter-selection process in 2016:
+
+        > 1. Decreased the number of eligible voters for each award from 130 to 100.
+        > 2. Limited the selection pool to "independent" media members (no
+           radio/television broadcasters or writers associated with a
+           particular team).
+        > 3. Required at least one voter per NBA market.
+
+        In total, the NBA's media-based awards have more meaning than ever and
+        understanding the process has become all the more important.
+
+        [1]: https://www.yahoo.com/news/nba-alters-voting-process-for-end-of-season-awards-in-quest-for-objectivity-190532014.html
+        [2]: https://twitter.com/NBA/status/812446292878102528?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E812446292878102528%7Ctwgr%5E%7Ctwcon%5Es1_&ref_url=https%3A%2F%2Fbleacherreport.com%2Farticles%2F2681705-nba-nbpa-agree-on-new-cba-latest-details-comments-reaction
+        [3]: http://sports.yahoo.com/news/the-rose-rule--why-it-needs-to-change-150439168.html
+        [4]: https://www.washingtonpost.com/news/sports/wp/2016/12/15/a-deeper-look-inside-the-nbas-new-collective-bargaining-agreement/?utm_term=.2497bf5a6a21
+        """
+    )
+
     st.subheader("🔍 Exploring the data")
-    Entry = Query()
+    st.markdown(
+        f"""
+        The NBA has  9 distinct media-chosen awards, each with its own number
+        of placements and scoring system (more on that later).
+        """
+    )
 
-    voters = defaultdict(int)
-    for row in DB:
-        voters[row["Voter"]] += 1
+    awards_df = pd.DataFrame(
+        [
+            ["Most Valuable Player", "MVP", "1st, 2nd, 3rd, 4th, 5th"],
+            ["Coach of the Year", "COY", "1st, 2nd, 3rd"],
+            ["Defensive Player of the Year", "DPOY", "1st, 2nd, 3rd"],
+            ["Rookie of the Year", "ROY", "1st, 2nd, 3rd"],
+            ["Most Improved Player", "MIP", "1st, 2nd, 3rd"],
+            ["6th Man of the Year", "6th", "1st, 2nd, 3rd"],
+            ["All-NBA Team", "All-NBA", "1st, 2nd, 3rd (5 spots each)"],
+            ["All-Rookie Team", "All-Rookie", "1st, 2nd (5 spots each)"],
+            ["All-Defensive Team", "All-Defensive", "1st, 2nd (5 spots each)"],
+        ],
+        columns=["Award", "ID", "Placements"],
+    )
 
-    most = sorted(voters, key=voters.get, reverse=True)  # type: ignore
-
-    data = []
-    for i, k in enumerate(most):
-        if i > 20:
-            break
-
-        q = DB.search(Entry.Voter == k)[0]
-        data.append([k, str(voters[k]), q["Affiliation"]])
-
-    df = pd.DataFrame(data, columns=["Name", "Votes", "Affiliation"])
+    st.dataframe(awards_df)
+    st.caption("The `ID` field corresponds to a table in our custom-made database.")
 
     st.markdown(
         f"""
-    Since 2014, there have been a total of `{len(voters)-1}` unique voters. Of
-    these individuals, the following have cast the most votes:
-    """
+        Use the drop-down menus below to browse the voting results for specific
+        award and year.
+        """
     )
 
-    st.dataframe(df)
-    st.caption(f"The maximum number of votes is `9` (awards) * `{len(YEARS)}` (years).")
+    col1, col2 = st.columns(2)
+
+    award = col1.selectbox(
+        "Select an award",
+        [
+            "MVP",
+            "COY",
+            "DPOY",
+            "ROY",
+            "MIP",
+            "6th",
+            "All-NBA",
+            "All-Rookie",
+            "All-Defensive",
+        ],
+    )
+    year = col2.selectbox("Select a year", YEARS)
+
+    Results = Query()
+    data = DB.search((Results.Award == award) & (Results.Year == str(year)))
+
+    results, headers = [], []
+    for i, row in enumerate(data):
+        if i == 0:
+            headers = row.keys()
+        results.append(row.values())
+
+    results_df = pd.DataFrame(results, columns=headers)
+    st.dataframe(results_df)
